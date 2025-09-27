c9f45b275acf5837ed542696ab1ab2569a0d0960
GrujicFilipRS <filipgrujicpersonal@gmail.com>
2025-09-27 19:08:50 +0200

    Vercel structure for server

--- server/api/index.py
+++ server/api/index.py
@@ -1,15 +1,15 @@
-import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+import os
 
 app = FastAPI()
 
+FRONTEND_URL: str = os.getenv('FRONTEND_URL', 'http://localhost:5173')
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins = [
-        "http://127.0.0.1:5173",
-        "http://localhost:5173"
-    ],
+    allow_origins = [FRONTEND_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -17,7 +17,4 @@ app.add_middleware(
 
 @app.get('/', status_code=200)
 async def root():
-    return {'message': 'Hello from FastAPI!'}
-
-if __name__ == "__main__":
-    uvicorn.run('main:app', host='127.0.0.1', port=8000, reload=True)
+    return JSONResponse({"message": "Hello from FastAPI!"})
