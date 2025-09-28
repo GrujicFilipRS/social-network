0b00c4e2b41fc35fd42fa4031b66cb8986fc94f3
GrujicFilipRS <filipgrujicpersonal@gmail.com>
2025-09-28 23:41:13 +0200

    /post/create_post/ api endpoint implemented

--- api/services/post.py
+++ api/services/post.py
@@ -14,6 +14,13 @@ from ..index import app
 
 from ..services.literals import PostLiterals
 
+
+class PostCreator(BaseModel):
+    title: str
+    body: str
+    status: str
+
+
 class AuthorizationHeader(BaseModel):
     Authorization: str
 
@@ -55,11 +62,11 @@ async def get_post(
             return JSONResponse(content=content, status_code=200)
         
         if not token:
-            return JSONResponse(content={'message': 'You are not authorized to view this post'}, status_code=403)
+            return JSONResponse(content={'message': 'You are not authorized to view this post'}, status_code=401)
         
         user_id: int = jwt_tokens.get_user_from_token(token)
         if user_id == -1 or user_id != post.user_id:
-            return JSONResponse(content={'message': 'You are not authorized to view this post'}, status_code=403)
+            return JSONResponse(content={'message': 'You are not authorized to view this post'}, status_code=401)
 
         return JSONResponse(content=content, status_code=200)
     
@@ -68,3 +75,45 @@ async def get_post(
 
     finally:
         db_sess.close()
+
+
+@app.post('/post/create_post/')
+def create_post(
+    data: PostCreator,
+    headers: Annotated[AuthorizationHeader, Header()]
+) -> JSONResponse:
+    
+    token: str = headers.Authorization
+
+    try:
+        if not token:
+            return JSONResponse(content={'message': 'You are not authorized to create posts'}, status_code=401)
+        
+        user_id: int = jwt_tokens.get_user_from_token(token)
+        if user_id == -1:
+            return JSONResponse(content={'message': 'You are not authorized to create posts'}, status_code=401)
+        
+        db_sess = create_session()
+
+        post = Post()
+        post.set_title(data.title)
+        post.set_body(data.body)
+        post.set_status(data.status)
+        post.user_id = user_id
+        post.created_at = datetime.now()
+
+        db_sess.add(post)
+        db_sess.commit()
+
+        content: dict = {
+            'message': 'Successfully created post',
+            'post': post.to_dict()
+        }
+
+        return JSONResponse(content=content, status_code=201)
+
+    except Exception as e:
+        return JSONResponse(content={'message': f'Error while getting post: {e}'}, status_code=500)
+    
+    finally:
+        db_sess.close() 
