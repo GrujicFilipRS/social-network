7f2ad8007f092869ffd65293e1f5598603e15901
GrujicFilipRS <filipgrujicpersonal@gmail.com>
2025-10-01 20:28:15 +0200

    Added endpoint /unfollow_user/

--- api/services/follow.py
+++ api/services/follow.py
@@ -5,6 +5,7 @@ from datetime import datetime, timezone
 from typing import Annotated
 
 from ..server.db.models.follows import Follow
+from ..server.db.models.users import User
 from ..server.db.db_session import create_session
 
 from ..server.utils import jwt_tokens
@@ -52,14 +53,17 @@ def follow_user(
         token: str = headers.Authorization
 
         if not token:
-            return JSONResponse(content={'message': f'Invalid authorization'}, status_code=401)
+            return JSONResponse(content={'message': 'Invalid authorization'}, status_code=401)
 
         user_id: int = jwt_tokens.get_user_from_token(token)
 
         if user_id == -1:
-            return JSONResponse(content={'message': f'Invalid authorization'}, status_code=401)
+            return JSONResponse(content={'message': 'Invalid authorization'}, status_code=401)
 
         db_sess = create_session()
+
+        if not db_sess.get(User, data.user_id):
+            return JSONResponse(content={'message': 'User not found'}, status_code=404)
 
         if db_sess.query(Follow).filter_by(follower_id=user_id, followed_id=data.to_follow_id).first():
             return JSONResponse(content={'message', 'You already follow this user'}, status_code=400)
@@ -72,10 +76,43 @@ def follow_user(
         db_sess.add(follow)
         db_sess.commit()
 
-        return JSONResponse(content={'message': f'Successfully followed user'}, status_code=201)
+        return JSONResponse(content={'message': 'Successfully followed user'}, status_code=201)
         
     except Exception as e:
         return JSONResponse(content={'message': f'Error while following: {e}'}, status_code=400)
     
     finally:
         db_sess.close()
+
+
+@router.delete('/unfollow_user/')
+def unfollow_user(
+    data: ToFollowData,
+    headers: Annotated[AuthorizationHeader, Header()]
+) -> JSONResponse:
+    try:
+        token: str = headers.Authorization
+
+        if not token:
+            return JSONResponse(content={'message': 'Invalid authorization'}, status_code=401)
+
+        user_id: int = jwt_tokens.get_user_from_token(token)
+
+        if user_id == -1:
+            return JSONResponse(content={'message': 'Invalid authorization'}, status_code=401)
+
+        db_sess = create_session()
+
+        follow = db_sess.query(Follow).filter_by(
+            follower_id=user_id,
+            followed_id=data.to_follow_id
+        ).first()
+
+        db_sess.delete(follow)
+        db_sess.commit()
+
+
+        return JSONResponse(content={'message': 'Successfully unfollowed user'}, status_code=200)
+    
+    except Exception as e:
+        return JSONResponse(content={'message', f'Unexpected error while unfollowing user: {e}'})
