e40792dd664e15c10d2a46d6ee6d137d76ccc810
GrujicFilipRS <filipgrujicpersonal@gmail.com>
2025-10-13 10:40:55 +0200

    Join page front created

---
+++ client/src/pages/Join.vue
@@ -0,0 +1,91 @@
+<script setup lang='ts'>
+
+import { ref, reactive } from 'vue';
+
+const signupMode = ref(true);
+
+interface SignupData {
+    username: string;
+    name: string;
+    password: string;
+    confirmPassword: string;
+};
+
+const formData = reactive<SignupData>({
+    username: '',
+    name: '',
+    password: '',
+    confirmPassword: ''
+});
+
+const submitSignup = () => {
+    console.log(formData);
+}
+
+</script>
+
+<template>
+
+<div class='join'>
+    <img src='/signup-image.jpg' />
+    <div class='form'>
+        <div class='inputs'>
+            <p><b>Username</b></p>
+            <input
+                type='text'
+                v-model='formData.username'
+                placeholder='Enter your username'
+            />
+
+            <p>Your name</p>
+            <input
+                v-show='signupMode'
+                type='text'
+                v-model='formData.name'
+                placeholder='Enter your name (not required)'
+            />
+            
+            <p><b>Your password</b></p>
+            <input
+                type='password'
+                v-model='formData.password'
+                placeholder='Enter your password'
+                
+            />
+
+            <p><b>Confirm password</b></p>
+            <input
+                v-show='signupMode'
+                type='password'
+                v-model='formData.confirmPassword'
+                placeholder='Confirm your password'
+            />
+        </div>
+        
+        <div class='buttons'>
+            <button
+                class='primary-btn'
+                @click='submitSignup'
+            >
+                {{ signupMode ? 'Sign up' : 'Log in' }}
+            </button>
+
+            <p>OR</p>
+
+            <button
+                class='secondary-btn'
+                @click='signupMode=!signupMode'
+            >
+                {{ signupMode ? 'Log in' : 'Sign up' }}
+            </button>   
+        </div>
+    </div>
+</div>
+
+</template>
+
+<style>
+
+@import url('./Join.css');
+
+</style>
--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,8 +1,10 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Hero from "../components/Hero.vue";
+import { createRouter, createWebHistory } from 'vue-router';
+import Hero from '../pages/Hero.vue';
+import Join from '../pages/Join.vue';
 
 const routes = [
-    { path: "/", component: Hero }
+    { path: '/', component: Hero },
+    { path: '/join', component: Join }
 ];
 
 const router = createRouter({
