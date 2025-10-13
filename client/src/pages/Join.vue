70d79fa0dac0641457f128832b41431045f419a5
GrujicFilipRS <filipgrujicpersonal@gmail.com>
2025-10-13 11:32:37 +0200

    Finished sign up fetch request

--- client/src/pages/Join.vue
+++ client/src/pages/Join.vue
@@ -1,8 +1,13 @@
 <script setup lang='ts'>
 
 import { ref, reactive } from 'vue';
+import { useRouter } from 'vue-router';
+import { API_ROUTE } from '../api';
+
+const router = useRouter();
 
 const signupMode = ref(true);
+const errorText = ref('');
 
 interface SignupData {
     username: string;
@@ -19,7 +24,42 @@ const formData = reactive<SignupData>({
 });
 
 const submitSignup = () => {
-    console.log(formData);
+    if (signupMode) {
+        if (formData.password == formData.confirmPassword)
+            handleSignup(formData.username.toLowerCase(), formData.password);
+    }
+    else {
+        handleLogin();
+    }
+}
+
+const handleSignup = (username: string, password: string) => {
+    fetch(`${API_ROUTE}/user/register/`, {
+        method: 'POST',
+        body: JSON.stringify({
+            username: username,
+            password: password
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(async (res) => {
+        if (res.status == 201) {
+            const data = await res.json();
+            localStorage.setItem('jwt', data['token']);
+            router.push('/feed');
+            return;
+        }
+    
+        const errMessage = (await res.json())['message'];
+        errorText.value = errMessage;
+    }).catch((err) => {
+
+    })
+}
+
+const handleLogin = () => {
+
 }
 
 </script>
@@ -37,7 +77,7 @@ const submitSignup = () => {
                 placeholder='Enter your username'
             />
 
-            <p>Your name</p>
+            <p v-show='signupMode'>Your name</p>
             <input
                 v-show='signupMode'
                 type='text'
@@ -53,13 +93,15 @@ const submitSignup = () => {
                 
             />
 
-            <p><b>Confirm password</b></p>
+            <p v-show='signupMode'><b>Confirm password</b></p>
             <input
                 v-show='signupMode'
                 type='password'
                 v-model='formData.confirmPassword'
                 placeholder='Confirm your password'
             />
+
+            <p class='red-color error-text'>{{errorText}}</p>
         </div>
         
         <div class='buttons'>
