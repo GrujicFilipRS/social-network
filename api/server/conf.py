474b69ee134a0acefa795a0890366dc314791d8b
GrujicFilipRS <filipgrujicpersonal@gmail.com>
2025-12-31 10:24:50 +0100

    Added JWT expiration hours as possible env variable

--- api/server/conf.py
+++ api/server/conf.py
@@ -2,7 +2,7 @@ import os, random
 
 class Config:
     SECRET_KEY: str = os.getenv('SECRET_KEY', f'social-{random.randint(0, 1000000)}')
-    JWT_EXPIRATION_HOURS: float = 24
+    JWT_EXPIRATION_HOURS: float = float(os.getenv('JWT_EXPIRATION_HOURS', '24'))
 
     SQLALCHEMY_TRACK_MODIFICATIONS: bool = False
     DBNAME: str = r'db/network.sqlite'
