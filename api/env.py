ffd88999f65431d7c86ea7665d31eec0e10b4361
GrujicFilipRS <filipgrujicpersonal@gmail.com>
2026-01-31 22:47:37 +0100

    Changed import scheme in env.py

--- api/env.py
+++ api/env.py
@@ -1,20 +1,21 @@
-import dotenv, os
+from dotenv import load_dotenv
+from os import getenv, path, environ, makedirs
 
-dotenv.load_dotenv()
+load_dotenv()
 
 def verify_env_vars() -> None:
     REQUIRED_ENV_VARS = (
         'CLOUDINARY_CLOUD_NAME',
         'CLOUDINARY_API_KEY',
         'CLOUDINARY_API_SECRET',
-        'SECRET_KEY'
+        'SECRET_KEY',
     )
 
     raise_exc: bool = False
     missing_vars: list[str] = []
 
     for var in REQUIRED_ENV_VARS:
-        if var not in os.environ.keys():
+        if var not in environ.keys():
             raise_exc = True
             missing_vars.append(var)
     
@@ -24,20 +25,20 @@ def verify_env_vars() -> None:
 verify_env_vars()
 
 def get_local_db_string(db_name: str) -> str:
-    base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-    db_dir = os.path.join(base_dir, 'db')
+    base_dir = path.abspath(path.join(path.dirname(__file__), '..'))
+    db_dir = path.join(base_dir, 'db')
 
-    os.makedirs(db_dir, exist_ok=True)
+    makedirs(db_dir, exist_ok=True)
 
-    return f'sqlite://{os.path.join(db_dir, f"{db_name.strip()}.sqlite")}?check_same_thread=False'
+    return f'sqlite://{path.join(db_dir, f"{db_name.strip()}.sqlite")}?check_same_thread=False'
 
 class Env:
-    CLOUDINARY_CLOUD_NAME = os.getenv('CLOUDINARY_CLOUD_NAME')
-    CLOUDINARY_API_KEY = os.getenv('CLOUDINARY_API_KEY')
-    CLOUDINARY_API_SECRET = os.getenv('CLOUDINARY_API_SECRET')
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    FRONTEND_URL = os.getenv('FRONTEND_URL', 'http://localhost:5173')
-    JWT_EXPIRATION_HOURS = float(os.getenv('JWT_EXPIRATION_HOURS', '24'))
-    DATABASE_URL = os.getenv('DATABASE_URL', get_local_db_string('network'))
-    FLASK_ENV = os.getenv('FLASK_ENV', 'development')
-    CLOUDINARY_PFP_FOLDER = os.getenv('CLOUDINARY_PFP_FOLDER', 'fastapi_uploads_pfp')
+    CLOUDINARY_CLOUD_NAME = getenv('CLOUDINARY_CLOUD_NAME')
+    CLOUDINARY_API_KEY = getenv('CLOUDINARY_API_KEY')
+    CLOUDINARY_API_SECRET = getenv('CLOUDINARY_API_SECRET')
+    SECRET_KEY = getenv('SECRET_KEY')
+    FRONTEND_URL = getenv('FRONTEND_URL', 'http://localhost:5173')
+    JWT_EXPIRATION_HOURS = float(getenv('JWT_EXPIRATION_HOURS', '24'))
+    DATABASE_URL = getenv('DATABASE_URL', get_local_db_string('network'))
+    FLASK_ENV = getenv('FLASK_ENV', 'development')
+    CLOUDINARY_PFP_FOLDER = getenv('CLOUDINARY_PFP_FOLDER', 'fastapi_uploads_pfp')
