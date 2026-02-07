a33bcd5bd3baddc310cb051d7287c49e8d2288b5
GrujicFilipRS <filipgrujicpersonal@gmail.com>
2026-02-07 12:26:43 +0100

    feat: integrate Tailwind CSS and enhance ChangePopup component
    - Added Tailwind CSS as a dependency and configured it in vite.config.ts.
    - Updated ChangePopup.vue to include an InputText component for username input.
    - Modified style.css to import Tailwind CSS and adjusted global styles.

--- client/src/components/ChangePopup.vue
+++ client/src/components/ChangePopup.vue
@@ -1,6 +1,7 @@
 <script setup lang='ts'>
 import Dialog from 'primevue/dialog';
 import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
 
 const props = defineProps<{
     visible: boolean,
@@ -16,7 +17,14 @@ const props = defineProps<{
         modal
         :header='props.title'
     >
-        
+        <div class='flex flex-col items-center'>
+            <span class='text-surface-500 dark:text-surface-400 block mb-2'>Update your username</span>
+
+            <div class='flex items-center gap-4 mb-4'>
+                <label for='username' class='font-semibold w-24'>Username</label>
+                <InputText id='username' class='flex-auto' autocomplete='off' />
+            </div>
+        </div>
 
         <div class='flex justify-end gap-2'>
             <Button
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,7 +11,10 @@ export default defineConfig({
         host: "0.0.0.0",
         port: 5173,
     },
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        tailwindcss()
+    ],
     build: {
         outDir: "dist",
     },
