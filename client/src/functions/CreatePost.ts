99ca94c41fda62c04de4244be2a7f68525ab0f19
GrujicFilipRS <filipgrujicpersonal@gmail.com>
2026-02-12 10:44:37 +0100

    refactor: update post redirection URL after successful post creation

--- client/src/functions/CreatePost.ts
+++ client/src/functions/CreatePost.ts
@@ -47,7 +47,7 @@ export const CreatePost = async (
             return;
         }
 
-        routerPush(`/post/${data.post.post_id}`);
+        routerPush(`/post?post_id=${data.post.id}`);
     }).finally(() => {
         setLoading(false);
     });
