77cc9f935ad66b135f9d7e3e6ce1fb466efba975
GrujicFilipRS <filipgrujicpersonal@gmail.com>
2026-03-13 16:52:22 +0100

    feat: implement header drawer buttons and implement primevue avatar component

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,28 +1,80 @@
 <script lang='ts' setup>
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 
 import { verifyUser } from '../api';
 
 import Drawer from 'primevue/drawer';
+import Avatar from 'primevue/avatar';
+import Button from 'primevue/button';
 
+const router = useRouter();
+
+const headerVisible = ref<boolean>(false);
 const drawerVisible = ref<boolean>(false);
 const pfpSource = ref<string>('/default-pfp.png');
 
 const fetchPfp = async () => {
     verifyUser().then(res => {
+        if (res.statusCode !== 401) headerVisible.value = true;
         if (res.result.user.pfp) pfpSource.value = res.result.user.pfp;
     });
 }
 
 fetchPfp();
 
+const Logout = () => {
+    // TODO implement frontend logout functionality
+}
+
 </script>
 
 <template>
-    <div id='header'>
-        <img :src='pfpSource' @click='drawerVisible = true' style='width: 75px; height: 75px; '>
-        <Drawer v-model:visible='drawerVisible'>
-            Test drawer
+    <div id='header' v-if='headerVisible'>
+        <Avatar
+            class='avatar'
+            :image='pfpSource'
+            @click='drawerVisible = true'
+            shape='circle'
+            style='width: 50px; height: 50px; cursor: pointer;'
+        />
+
+        <Drawer
+            v-model:visible='drawerVisible'
+            position='right'
+            header='Quick options'
+            class='header-drawer'
+        >
+            <Button
+                class='header-btn'
+                icon='pi pi-user'
+                label='View profile'
+                severity='secondary'
+                style='width: 100%;'
+                @click='() => { router.push("/profile"); drawerVisible = false; }'
+            />
+
+            <Button
+                class='header-btn'
+                icon='pi pi-user-edit'
+                label='Edit profile'
+                severity='secondary'
+                style='width: 100%;'
+                @click='() => { router.push("/edit_profile"); drawerVisible = false; }'
+            />
+
+            <Button
+                class='header-btn'
+                icon='pi pi-sign-out'
+                label='Log out'
+                severity='danger'
+                style='width: 100%;'
+                @click='Logout'
+            />
         </Drawer>
     </div>
 </template>
+
+<style>
+@import url('./Header.css');
+</style>
