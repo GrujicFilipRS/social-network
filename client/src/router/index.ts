09432fa40041872a8f4cab337e5b70378a7f69fe
GrujicFilipRS <filipgrujicpersonal@gmail.com>
2026-04-16 20:32:45 +0200

    feat: add shorter, more shareable /u/{username} route that redirects to profile page

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import type { RouteRecordRaw, RouteLocationGeneric } from 'vue-router';
 import Hero from '../pages/Hero.vue';
 import Join from '../pages/Join.vue';
 import Feed from '../pages/Feed.vue';
@@ -7,7 +8,16 @@ import EditProfile from '../pages/EditProfile.vue';
 import CreatePost from '../pages/CreatePost.vue';
 import ViewPost from '../pages/ViewPost.vue';
 
-const routes = [
+const routes: RouteRecordRaw[] = [
+    {
+        path: '/u/:username',
+        redirect: (to: RouteLocationGeneric) => {
+            return {
+                path: '/profile',
+                query: { user: to.params.username as string }
+            };
+        }
+    },
     { path: '/:pathMatch(.*)*', redirect: '/' },
     { path: '/', component: Hero },
     { path: '/join', component: Join },
