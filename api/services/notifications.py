9b4d4370599409516bcc2b82873a971562324ffc
GrujicFilipRS <filipgrujicpersonal@gmail.com>
2026-04-22 16:31:44 +0200

    feat: implement websocket authentication using JWT

--- api/services/notifications.py
+++ api/services/notifications.py
@@ -1,14 +1,20 @@
+from uuid import UUID
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
+from utils import JWT
 
 router = APIRouter()
 
 @router.websocket('/')
-async def websocket(websocket: WebSocket):
+@JWT.required_auth_websocket
+async def websocket(
+    websocket: WebSocket,
+    user_id: UUID | None = None
+):
     await websocket.accept()
     
     try:
         while True:
             data = await websocket.receive_text()
-            await websocket.send_text(f'Echo: {data}')
+            await websocket.send_text(f'Echo {user_id}: {data}')
     except WebSocketDisconnect:
         print('Client disconnected')
--- api/utils/jwt_tokens.py
+++ api/utils/jwt_tokens.py
@@ -1,5 +1,5 @@
 from functools import wraps
-from fastapi import Request
+from fastapi import Request, WebSocket
 from fastapi.responses import JSONResponse
 import jwt
 from datetime import datetime, timezone, timedelta
@@ -117,3 +117,44 @@ class JWT:
             secure=Env.FLASK_ENV == 'production',
             expires=Env.JWT_EXPIRATION_HOURS * 3600
         )
+    
+    @staticmethod
+    def required_auth_websocket(func):
+        @wraps(func)
+        async def async_wrapper(*args, **kwargs):
+            websocket = kwargs.get('websocket')
+
+            if not isinstance(websocket, WebSocket):
+                raise RuntimeError(
+                    '@required_auth_websocket requires `websocket` parameter to be of type WebSocket'
+                )
+
+            websocket.accept()
+
+            token = JWT.get_cookie_from_websocket(websocket)
+            if not token:
+                await websocket.close(code=1008, reason='Missing Authorization Cookie')
+                return
+
+            user_id: UUID | None = JWT.decode_token(token)
+            if not user_id:
+                await websocket.close(code=1008, reason='Invalid or expired token')
+                return
+
+            kwargs['user_id'] = user_id
+
+            return await func(*args, **kwargs)
+
+        return async_wrapper
+    
+    @staticmethod
+    def get_cookie_from_websocket(websocket: WebSocket) -> str | None:
+        cookie_header = websocket.headers.get('cookie')
+        if not cookie_header:
+            return None
+
+        cookies = dict(
+            item.strip().split('=', 1)
+            for item in cookie_header.split(';')
+        )
+        return cookies.get(JWT.AUTH_COOKIE_NAME)
