d419829dedffb09109406e1ac37632bd014e972d
GrujicFilipRS <filipgrujicpersonal@gmail.com>
2026-05-17 19:04:11 +0200

    feat: add endpoint for having read notifications

--- api/services/notifications.py
+++ api/services/notifications.py
@@ -1,5 +1,6 @@
 from uuid import UUID
 from fastapi import APIRouter, Request, WebSocket
+from fastapi.responses import JSONResponse
 from models import Notification
 from utils import JWT, ConnectionController
 from db import DBSessionManager
@@ -43,3 +44,26 @@ async def get_unread_notifications(
             notification.to_dict()
             for notification in notifications
         ]
+        
+
+@router.post('/read_notification/{notification_id}/')
+@JWT.require_auth
+async def read_notification(
+    request: Request,
+    notification_id: UUID,
+    user_id: UUID | None = None
+):
+    with DBSessionManager() as db_sess:
+        notification = (
+            db_sess.query(Notification)
+            .filter_by(id=notification_id, receiver_id=user_id)
+            .first()
+        )
+
+        if not notification:
+            return JSONResponse(status_code=404, content={'error': 'Notification not found'})
+
+        notification.seen = True
+        db_sess.commit()
+
+        return JSONResponse(status_code=200, content={'message': 'Notification marked as read'})
