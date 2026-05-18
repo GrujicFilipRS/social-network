72ddb1042b0a2eb8ac7dd7cdc7ebca1f27228a2a
GrujicFilipRS <filipgrujicpersonal@gmail.com>
2026-05-18 22:52:42 +0200

    refactor: make WorkerShareController code use correct typing

--- api/utils/worker_share_controller.py
+++ api/utils/worker_share_controller.py
@@ -2,27 +2,27 @@ from redis.asyncio import Redis
 from env import Env
 
 class WorkerShareController:
-    redis_client: Redis = None
+    redis_client: Redis | None = None
     
     @staticmethod
     def init():
         redis_url = Env.REDIS_URL
         
-        WorkerShareController.redis_client = Redis.from_url(redis_url)
+        WorkerShareController.redis_client = Redis.from_url(redis_url, decode_responses=True)
         print(f'Redis client initialized successfully at {redis_url}')
     
     @staticmethod
-    def set(key: str, value: str):
+    async def set(key: str, value: str):
         if WorkerShareController.redis_client is None:
             raise RuntimeError('WorkerShareController not yet initialized')
         
-        WorkerShareController.redis_client.set(key, value)
+        await WorkerShareController.redis_client.set(key, value)
     
     @staticmethod
-    def get(key: str) -> str | None:
+    async def get(key: str) -> str | None:
         if WorkerShareController.redis_client is None:
             raise RuntimeError('WorkerShareController not yet initialized')
         
-        value = WorkerShareController.redis_client.get(key)
+        value: str | None = await WorkerShareController.redis_client.get(key)
         
-        return value.decode('utf-8') if value else None
+        return value.strip() if value is not None else None
