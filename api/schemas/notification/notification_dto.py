bc0044fa8743a662494ceeb44b4f9c4231f47108
GrujicFilipRS <filipgrujicpersonal@gmail.com>
2026-08-18 16:28:13 +0200

    feat: implement notification dto

--- api/schemas/notification/notification_dto.py
+++ api/schemas/notification/notification_dto.py
@@ -3,7 +3,26 @@ from __future__ import annotations
 from models import Notification
 from pydantic import BaseModel
 
+from ..user.user_dto import UserDTO
+
 
 class NotificationDTO(BaseModel): # TODO
-    def to_dto(notification: Notification) -> NotificationDTO:
-        return NotificationDTO()
+    id: str
+    receiver: UserDTO
+    sender: UserDTO
+    object_type: str
+    object_id: str
+    seen: bool
+    
+    def to_dto(notification: Notification | NotificationDTO) -> NotificationDTO:
+        if isinstance(notification, NotificationDTO):
+            return notification
+        
+        return NotificationDTO (
+            id=notification.id,
+            receiver=UserDTO.to_dto(notification.receiver),
+            sender=UserDTO.to_dto(notification.sender),
+            object_type=notification.object_type,
+            object_id=notification.object_id,
+            seen=notification.seen
+        )
